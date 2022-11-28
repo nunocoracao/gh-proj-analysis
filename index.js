985b5610c52759c9db467942fa487b25c35bff86
Nuno Coração <nunocoracao@gmail.com>
2022-11-28 15:01:18 +0000

    stuff

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var utils = require('./src/utils');
 //var repo = utils.readRepoArgs(process.argv)
 var repo = 'https://github.com/BretFisher/example-voting-app.git'
 
+//this needs to be timout'd
+
 utils.print(repo);
 utils.cloneRepo(repo, (repoName) => {
     console.log(repoName)
     var struct = utils.scanFolder(repoName)
+    //process
     utils.saveToFile(struct, 'test.json')
 })
 
@@ -17,3 +20,11 @@ utils.cloneRepo(repo, (repoName) => {
 //var test = process.argv
 //utils.print(test);
 //utils.saveToFile(test, 'test.json');
+
+
+/*
+
+Dockerfile doesn't need to be called a Dockerfile - how to indentify?
+Compose file - how to identfy?
+
+*/
