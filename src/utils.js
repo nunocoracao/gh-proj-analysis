faa41705875782f2072f5cd6325cf09594c5dddc
Nuno Coração <nunocoracao@gmail.com>
2022-12-13 21:59:38 +0000

    individual git repo analytics done

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,8 @@ const TEMP_DIR = "./temp/"
 const OUTPUT_DIR = "./outputs/"
 const REPOS_FILE = "./oldrepos.json"
 
+const dockerfile_regex = /[Dd]ockerfile.*/;
+const compose_regex = /.*[Cc]ompose.*ya*ml/;
 
 module.exports = {
 
@@ -42,29 +44,85 @@ module.exports = {
     },
 
     scanFolder: function (folder) {
-        var path = TEMP_DIR+folder
+        var path = folder
         var struct = {}
-        var files = fs.readdirSync(TEMP_DIR+folder, {withFileTypes:true})
+        var files = fs.readdirSync(TEMP_DIR + folder, { withFileTypes: true })
 
         this.print('scanning ' + path)
 
-        for(var i in files){
+        for (var i in files) {
             struct[files[i].name] = {}
+            struct[files[i].name].name = files[i].name
             struct[files[i].name].path = path + "/" + files[i].name
             struct[files[i].name].isDir = files[i].isDirectory()
-            if(struct[files[i].name].isDir){
+            struct[files[i].name].isDockerfile = files[i].name.match(dockerfile_regex) ? true : false;
+            struct[files[i].name].isComposefile = files[i].name.match(compose_regex) ? true : false;
+            if (struct[files[i].name].isDir) {
                 struct[files[i].name].children = this.scanFolder(folder + '/' + files[i].name)
             }
         }
         return struct
     },
 
-    processStruct: function(struct) {
+    processStruct: function (struct) {
         this.print('processing structure for important files')
         var obj = {
-            struct : struct
+            results: this.processStructAux(struct),
+            struct: struct
         }
         return obj
+    },
+
+    processStructAux: function (struct) {
+        var res = {
+            dockerfile: {
+                exist: false,
+                count: 0,
+                rootCount: 0,
+                subFolderCount: 0,
+                filepaths: []
+            },
+            composefile: {
+                exist: false,
+                count: 0,
+                rootCount: 0,
+                subFolderCount: 0,
+                filepaths: []
+            }
+        }
+
+        var directories = []
+
+        for(var i in struct){
+
+            if(struct[i].isDockerfile){
+                res.dockerfile.exist = true
+                res.dockerfile.count++
+                res.dockerfile.rootCount++
+                res.dockerfile.filepaths.push(struct[i].path)
+            }else if(struct[i].isComposefile){
+                res.composefile.exist = true
+                res.composefile.count++
+                res.composefile.rootCount++
+                res.composefile.filepaths.push(struct[i].path)
+            } else if(struct[i].isDir){
+               directories.push(struct[i])
+            }
+        }
+
+        for(var d in directories){
+            var temp = this.processStructAux(directories[d].children)
+            res.dockerfile.exist = temp.dockerfile.exist? true : res.dockerfile.exist
+            res.dockerfile.count += temp.dockerfile.count
+            res.dockerfile.subFolderCount += temp.dockerfile.rootCount + temp.dockerfile.subFolderCount
+            res.dockerfile.filepaths = res.dockerfile.filepaths.concat(temp.dockerfile.filepaths)
+            res.composefile.exist = temp.composefile.exist? true : res.composefile.exist
+            res.composefile.count += temp.composefile.count
+            res.composefile.subFolderCount += temp.composefile.rootCount + temp.composefile.subFolderCount
+            res.composefile.filepaths = res.composefile.filepaths.concat(temp.composefile.filepaths)
+        }
+
+        return res
     },
 
     processOverallResults: function () {
