df240e564112c0673191685a4c93c9f2e43858fd
Nuno Coração <nunocoracao@gmail.com>
2022-12-14 08:57:13 +0000

    updated results

--- src/ghCrawler.js
+++ src/ghCrawler.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const LIMIT_PER_REQUEST = 1000
+const LIMIT_PER_REQUEST = 2000
 const INTERVAL = 5000
 
 var crawlLikeABoss = (query, opts, callback, preData, page) => {
